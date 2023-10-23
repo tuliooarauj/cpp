d671eb2fea0f48a9e7059cb2fb2c6ff816b9d857
tuliooarauj <127243520+tuliooarauj@users.noreply.github.com>
2023-10-23 14:56:39 -0300

    first commit

---
+++ firstCode.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout<< "Hello World\n First code!" << endl; // endl ou \n p/ quebra de linha
+
+    system("pause");
+
+    return 0;
+}
